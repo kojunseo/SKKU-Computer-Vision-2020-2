149553676210d85438612f6a9d327a47dfa77eef
고준서 <sta06167@naver.com>
2020-12-19 06:22:35 +0900

    Delete Some Files

--- assignment_1/CV_A1_2018310412/computational_time.py
+++
@@ -1,9 +0,0 @@
-import time
-
-class TimeModule:
-    def __init__(self):
-        self.now = time.time()
-
-    def end_print(self, name):
-        computational_time = time.time() - self.now
-        print(f"Computational Time of {name} : {computational_time}")
